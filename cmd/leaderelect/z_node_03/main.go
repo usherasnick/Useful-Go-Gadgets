b1122c0e01751ddc74c6f2b0c809b2b7d38110c2
usherasnick <461826470@qq.com>
2021-02-24 16:26:06 +0800

    Add leaderelect

---
+++ cmd/leaderelect/z_node_03/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	leaderelect "github.com/usherasnick/Useful-Go-Gadgets/leader-elect-by-zk"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+
+	cfg := &leaderelect.ElectorCfg{
+		ZkEndpoints: []string{"127.0.0.1:2181", "127.0.0.1:2182", "127.0.0.1:2183"},
+		Heartbeat:   2,
+	}
+	e := leaderelect.NewElector(cfg)
+	go e.ElectLeader("z_node_03")
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	for range sigCh {
+		e.Close()
+		break
+	}
+}
